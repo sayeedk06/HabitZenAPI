92a291f877f1c55eb0abbe3bbfd4aff08ee43fe9
sayeedk06 <sayeed.k06@gmail.com>
2024-06-01 11:51:39 +1000

    added update route and getting habits using habitid

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,42 @@ var router = express.Router();
 var authorize = require('../middleware/auth')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({unit: "IFN666"});
+router.get('/', function (req, res, next) {
+  res.json({ unit: "IFN666" });
 });
 
-router.get('/api/habits', function(req, res, next) {
-  const query = req.db.from("habits").select("*")
-  return query.then(result => {
-    res.json(result);
-  })
-   
+
+router.get('/api/habits/', function (req, res, next) {
+  const idhabits = req.query.idhabits;
+
+  if (idhabits) {
+    const query = req.db.from('habits').select('*').where('idhabits', '=', idhabits)
+    return query.then(result => res.json(result)
+    )
+  } else {
+    const query = req.db.from("habits").select("*")
+    return query.then(result => {
+      res.json(result);
+    })
+  }
+
 });
-router.get('/api/habits/:userId', function(req, res, next) {
+
+// router.get('/api/habits/', function(req, res, next) {
+//   const query = req.db.from("habits").select("*")
+//   return query.then(result => {
+//     res.json(result);
+//   })
+
+// });
+
+
+router.get('/api/habits/:userId', function (req, res, next) {
 
   const userId = req.params.userId;
 
-  if(!userId) {
-    res.status(400).json( {
+  if (!userId) {
+    res.status(400).json({
       error: true,
       message: "Request body incomplete - please check the missing input field"
     });
@@ -30,10 +49,12 @@ router.get('/api/habits/:userId', function(req, res, next) {
   return query.then(result => {
     res.json(result);
   })
-   
+
 });
 
-router.post("/api/habits", function(req, res, next) {
+
+
+router.post("/api/habits", function (req, res, next) {
 
   const name = req.body.name
   const description = req.body.description;
@@ -54,17 +75,17 @@ router.post("/api/habits", function(req, res, next) {
   console.log('end ' + endTerm)
   console.log('userid ' + userId)
 
-  if (!name || !category || !goals|| !period || !tags || !startTerm || !endTerm || !userId) {
-      res.status(400).json( {
-        error: true,
-        message: "Request body incomplete - please check the missing input field"
-      });
-      return;
+  if (!name || !category || !goals || !period || !tags || !startTerm || !endTerm || !userId) {
+    res.status(400).json({
+      error: true,
+      message: "Request body incomplete - please check the missing input field"
+    });
+    return;
   }
 
-  return req.db.from("habits").insert({name, description, category, goals, period, tags, startTerm, endTerm, userId}).then(function (result) {
+  return req.db.from("habits").insert({ name, description, category, goals, period, tags, startTerm, endTerm, userId }).then(function (result) {
     console.log(result)
-  res.json({success: true, message: 'Successfully executed'})
+    res.json({ success: true, message: 'Successfully executed' })
   })
   return;
 });
@@ -81,14 +102,14 @@ router.post("/api/habits", function(req, res, next) {
 //     return;
 //   }
 //   return req.db.from("habits").where("idhabits", "=", habitId).del()
-  
+
 // });
 
-router.delete("/api/habits/delete/:habitId", function(req, res, next) {
+router.delete("/api/habits/delete/:habitId", function (req, res, next) {
 
   const habitId = req.params.habitId;
   if (!habitId) {
-    res.status(400).json( {
+    res.status(400).json({
       error: true,
       message: "Request body incomplete - please check the missing input field"
     });
@@ -96,9 +117,43 @@ router.delete("/api/habits/delete/:habitId", function(req, res, next) {
   }
   return req.db.from("habits").where("idhabits", "=", habitId).del().then(function (result) {
     console.log(result)
-    res.json({success: true, message: 'Successfully executed'})
+    res.json({ success: true, message: 'Successfully executed' })
   })
 }
 
 )
+
+router.put("/api/habits/update/:habitId", function (req, res, next) {
+  const habitId = req.params.habitId;
+  const name = req.body.name
+  const description = req.body.description;
+  const category = req.body.category;
+  const goals = req.body.goals;
+  const period = req.body.period;
+  const tags = req.body.tags;
+  const startTerm = req.body.startTerm;
+  const endTerm = req.body.endTerm;
+
+  if (!habitId) {
+    res.status(400).json({
+      error: true,
+      message: "Request body incomplete - please check the missing input field"
+    });
+    return;
+  }
+  return req.db.from("habits").where("idhabits", "=", habitId).update({
+    name: name,
+    description: description,
+    category: category,
+    goals: goals,
+    period: period,
+    tags: tags,
+    startTerm: startTerm,
+    endTerm: endTerm
+
+  }).then(function (result) {
+    console.log(result)
+    res.json({ success: true, message: 'Successfully executed' })
+  })
+});
 module.exports = router;
